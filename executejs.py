8ae65bc962eba9ac15b425fe9df2dbeadcba3443
Jaspreet Lall <67714004+jassi-lall@users.noreply.github.com>
2023-09-02 09:00:19 +0100

    Add files via upload

---
+++ executejs.py
@@ -0,0 +1,23 @@
+"""
+Demonstrates how you can execute JavaScript code embedded in HTML responses using selenium and headless chromemium driver
+Useful for scraping content that requires js.
+"""
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+
+def executeJS(url):
+    options = Options()
+    options.add_argument('--headless=new') 
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    page_source = driver.page_source
+    driver.close()
+    return page_source
+
+page_source = executeJS('https://givingmatters.civicore.com/nonprofits')
+soup = BeautifulSoup(page_source, 'html.parser')
+m = soup.find(id='main')
